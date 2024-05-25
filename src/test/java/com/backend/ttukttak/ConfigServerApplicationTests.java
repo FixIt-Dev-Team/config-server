422e2a9d3a5c3b8d999ba3f08fd7e727f51b691c
SeungHyeon <namsh1125@naver.com>
2024-05-25 12:38:41 +0900

    Init: 프로젝트 초기 설정

---
+++ src/test/java/com/backend/ttukttak/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.ttukttak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
